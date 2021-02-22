0100b33902d766c162d30c7fd16a549eb4157d02
Brendan Mulholland <github@bmulholland.ca>
2021-02-22 19:18:06 +0100

    Fix Rubocop

--- jobs/process_contract_scan_result_job.rb
+++ jobs/process_contract_scan_result_job.rb
@@ -19,13 +19,17 @@ class ProcessContractScanResultJob
     params(contract_info: ContractScanResult,
            attachment: EmailProvider::Attachment).void
   end
-  def self.save_detected_contract_and_cache_attachment(contract_info, attachment)
+  def self.save_detected_contract_and_cache_attachment(
+    contract_info, attachment
+  )
     # Exclamation marks mean an error will be thrown if the operation fails
     Attachment.create!(
       external_id: attachment.id,
       email: Email.create!(
         external_id: attachment.message.id,
-        conversation: Conversation.create!(external_id: attachment.message.conversation_id),
+        conversation: Conversation.create!(
+          external_id: attachment.message.conversation_id,
+        ),
       ),
     )
   end
