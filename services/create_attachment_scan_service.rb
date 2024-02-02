56397d6cab11d7acd0ce514b96f534e6bf7b154c
Hiemanshu Sharma <hiemanshu@gmail.com>
2024-02-02 21:21:09 +0530

    TIDY Add information about attachment scan service

--- services/create_attachment_scan_service.rb
+++ services/create_attachment_scan_service.rb
@@ -11,5 +11,12 @@ class CreateAttachmentScanService
     # Intentionally blank - this would be the logic that kicks off the scan
     # But that has side effects and this is just a sample project, so we're
     # faking it
+    #
+    # Uploads file to Attachment Scanning Service, returns metadata about
+    # inferred attachment info
+    # Example in: assessment.rb:51
+    #
+    # If `Email` doesn't exist, would create an `Email` object for the attachment
+    # to belong to
   end
 end
